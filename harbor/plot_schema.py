7b32d3c768da0db3d988f5147b4d9f627056b290
Alexander Matthew Payne <alexpayne1997@gmail.com>
2024-02-07 09:31:54 -0500

    add schema for inputs to plotting

--- harbor/plot_schema.py
+++ harbor/plot_schema.py
@@ -1,1 +1,23 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+
+class RocCurve(BaseModel):
+    """
+    ROC curve
+    """
+
+    id: str = Field(..., description="The name of the model assessed in this curve")
+    fpr: list[float] = Field(..., description="False positive rate (x-axis)")
+    tpr: list[float] = Field(..., description="True positive rate (y-axis)")
+    auc: float = 0.0
+
+
+class PrecisionRecallCurve(BaseModel):
+    """
+    Precision recall curve
+    """
+
+    id: str = Field(..., description="The name of the model assessed in this curve")
+    precision: list[float] = Field(..., description="Precision (x-axis)")
+    recall: list[float] = Field(..., description="Recall (y-axis)")
+    auc: float = 0.0
