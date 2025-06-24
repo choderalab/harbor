89e83874ee312a4eba8f558474875f6ed2cd1e04
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-06-24 15:04:38 -0400

    test harbor cli

--- harbor/tests/test_cli.py
+++ harbor/tests/test_cli.py
@@ -1,12 +1,21 @@
 # make sure cli loads without error
+# this code is shamelessly copied from asapdiscovery-cli/asapdiscovery/cli/tests/test_meta_cli.py
 
 from harbor.cli import cli as harbor_cli
+from click.testing import CliRunner
+import traceback
 
 
-def test_cli():
-    """Test that the CLI loads without error."""
-    try:
-        harbor_cli()
-    except Exception as e:
-        assert False, f"CLI failed to load: {e}"
-    assert True, "CLI loaded successfully"
+def click_success(result):
+    if result.exit_code != 0:  # -no-cov-  (only occurs on test error)
+        print(result.output)
+        traceback.print_tb(result.exc_info[2])
+        print(result.exc_info[0], result.exc_info[1])
+    return result.exit_code == 0
+
+
+def test_toplevel_runnable():
+    runner = CliRunner()
+    args = ["--help"]
+    result = runner.invoke(harbor_cli, args)
+    assert click_success(result)
